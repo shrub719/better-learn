f283c0cbd6955c87b9149972df90721685a6d6cf
shrub <shrub719@gmail.com>
2025-07-07 09:47:55 +0100

    added video skipping test

--- src/content.js
+++ src/content.js
@@ -180,8 +180,78 @@ function setVideoSpeed(speed) {
     document.querySelector("video").playbackRate = speed;
 }
 
+// ===== VIDEO LESSONS =====
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function skipToQuestion(seconds) {
+    setVideoSpeed(10.0);
+    await sleep((seconds - 1) * 1000);
+    setVideoSpeed(1.0);
+}
+
+function durationToSeconds(isoDuration) {
+    const regex = /PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+(?:\.\d+)?)S)?/;
+    const match = isoDuration.match(regex);
+  
+    if (!match) return 0;
+  
+    const hours = parseFloat(match[1] || 0);
+    const minutes = parseFloat(match[2] || 0);
+    const seconds = parseFloat(match[3] || 0);
+  
+    return hours * 3600 + minutes * 60 + seconds;
+  }
+
+async function skipVideoLesson(url) {
+    console.log("# skipVideoLesson");
+
+    setVideoSpeed(1.0);
+
+    const path = new URL(url).pathname.split('/').filter(Boolean);
+    const [moduleUniqueCode, subsectionUniqueCode] = path.slice(-2);
+    const getLessonBody = {
+        "operationName": "GetVideoLesson",
+        "variables": {
+            "moduleUniqueCode": moduleUniqueCode,
+            "subsectionUniqueCode": subsectionUniqueCode,
+            "uniqueCode": video.uniqueCode,
+            "inVideoQuestionOrder": [
+                {
+                    "field": "TRIGGER_TIME",
+                    "direction": "ASC"
+                }
+            ]
+        },
+        "query": "query GetVideoLesson($uniqueCode: String!, $subsectionUniqueCode: String!, $moduleUniqueCode: String!, $inVideoQuestionFilter: InVideoQuestionFilter, $inVideoQuestionOrder: [InVideoQuestionOrdering]) {\n  videoLesson(\n    filter: {uniqueCode: $uniqueCode, subsectionUniqueCode: $subsectionUniqueCode, moduleUniqueCode: $moduleUniqueCode}\n  ) {\n    id\n    module {\n      id\n      uniqueCode\n      title\n      examBoardSubject {\n        subject: subjectObject {\n          qualification {\n            uniqueCode\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    subsection {\n      id\n      name\n      uniqueCode\n      __typename\n    }\n    wistiaId\n    uniqueCode\n    title\n    length\n    duration\n    extraContent\n    skipRecapStartTime\n    preVideoQuestions {\n      __typename\n      id\n      quizContent {\n        ...UnmarkedQuestion\n        __typename\n      }\n    }\n    inVideoQuizQuestions(\n      filter: $inVideoQuestionFilter\n      order: $inVideoQuestionOrder\n    ) {\n      __typename\n      id\n      triggerTime\n      postQuestionResumeTime\n      quizContent {\n        __typename\n        ...UnmarkedQuestion\n      }\n    }\n    __typename\n  }\n}\n\nfragment UnmarkedQuestion on QuizContent {\n  __typename\n  id\n  stem\n  quizDefinition {\n    __typename\n    questions {\n      ...UnmarkedQuestionPart\n      __typename\n    }\n  }\n}\n\nfragment UnmarkedQuestionPart on QuizQuestion {\n  __typename\n  ... on MultipleChoiceQuestion {\n    question\n    description\n    image\n    topImage\n    options {\n      text\n      image\n      __typename\n    }\n    __typename\n  }\n  ... on MultiMultipleChoiceQuestion {\n    question\n    description\n    image\n    topImage\n    options {\n      text\n      image\n      __typename\n    }\n    __typename\n  }\n  ... on TextQuestion {\n    question\n    description\n    image\n    topImage\n    beforeText\n    afterText\n    __typename\n  }\n  ... on NumericalQuestion {\n    question\n    description\n    image\n    topImage\n    beforeText\n    afterText\n    __typename\n  }\n  ... on MathsQuestion {\n    question\n    description\n    image\n    topImage\n    __typename\n  }\n  ... on MultipleInputQuestion {\n    questionSegments {\n      type: __typename\n      ... on MultipleInputQuestionText {\n        text\n        __typename\n      }\n      ... on MultipleInputQuestionBlank {\n        fieldIndex\n        __typename\n      }\n    }\n    description\n    image\n    topImage\n    __typename\n  }\n  ... on ChemistryQuestion {\n    question\n    description\n    image\n    topImage\n    __typename\n  }\n  ... on DropdownQuestion {\n    question\n    description\n    image\n    topImage\n    dropdownOptions\n    __typename\n  }\n  ... on DrawQuestion {\n    question\n    description\n    image\n    topImage\n    drawOn\n    __typename\n  }\n  ... on OpenEndedQuestion {\n    question\n    description\n    image\n    topImage\n    __typename\n  }\n}"
+    };
+    const lesson = await apiFetch(auth, getLessonBody);
+    const questions = lesson.data.videoLesson.inVideoQuizQuestions;
+
+    let prevTimestamp = 0;
+    for (question of questions) {
+        nextTimestamp = durationToSeconds(question.triggerTime);
+        await skipToQuestion(nextTimestamp - prevTimestamp);
+        prevTimestamp = nextTimestamp;
+    }
+}
+
+function checkVideoLesson() {
+    console.log("# checkVideoLesson");
+    const url = window.location.href;
+    if (url.slice(-5) == "video") {
+        skipVideoLesson(url);
+    }
+}
+
 // ===== LISTENERS =====
 
 console.log("# hey im here!")
 completeAssignment();  // wait for message from content
 setVideoSpeed(3.5);
+
+// is this run on every page reload?
+// check if the url is a video, then run checkVideoLesson
+checkVideoLesson();
--- utils/todo.md
+++ utils/todo.md
@@ -1,8 +1,8 @@
 ## To do
 
-- Popup
+- [ ] Popup
     - Only trigger if button in popup pressed, otherwise too many requests -> rate limited
-- Video skip
+- [ ] Video skip
     - True skip seems out of reach for now
     - Can skip watching the video and only make the user answer the questions though!!
     - Need to understand the API calls for that though
@@ -12,4 +12,4 @@
             - This happens client side after the InVideoQuestions are fetched, so it's all ugly minified code. Maybe not
         - Intercept the request and change trigger times to be at 0
         - Automatically make the speed super high except at question checkpoints
-- Take the other extension's job of setting video speed
+- [ ] Take the other extension's job of setting video speed
